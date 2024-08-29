7368751d1ba16de82541ac5cd94f4e85be503db8
wa1ker38552 <walker.alt.38552@gmail.com>
2024-08-29 12:54:10 -0700

    fixed icon colors, made the curl commands actually execute

--- js/script.js
+++ js/script.js
@@ -10,8 +10,17 @@ async function renderTerminal() {
 
     while (1) {
         website = random.choice(websites)
-        parent.append(dcreate("div", "", `root:~# curl <span style='color: var(--accent-secondary)'>${website}</span>`))
-        parent.scrollTop = parent.scrollHeight;
-        await time.sleep(500)
+        // cors proxy
+        requestRaw("https://api.codetabs.com/v1/proxy?quest="+website)
+            .then(response => response.text())
+            .then(response => {
+                parent.append(dcreate("div", "", `root:~# curl <span style='color: var(--accent-secondary)'>${website}</span>`))
+                let websiteContent = dcreate("div")
+                websiteContent.style.color = "var(--text-dim)"
+                websiteContent.textContent = `"${response.slice(0, 150).trim()}..."`
+                parent.append(websiteContent)
+                parent.scrollTop = parent.scrollHeight;
+            })
+        await time.sleep(1000)
     }
 }
