9805726535f6c4e61c49340a12d36b567902e641
Timothy Wei <timswei@gmail.com>
2024-08-29 20:26:35 -0700

    cleanup code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-/*async function loadWebsites() {
-    const response = await fetch('data/websites.txt'); 
-    const text = await response.text();
-    return text.split("\n").filter(website => website.trim() !== "");  // Split and remove empty lines
-}*/
-
 async function renderTerminal() {
     const parent = dquery("#terminalContent")
     // const websites = await loadWebsites()
-
-    let is_done_loading = false;
 
     while (1) {
         website = random.choice(websites)
@@ -31,30 +23,6 @@ async function renderTerminal() {
 
         let formattedResponse = formatResponse(text)
         await typeEffect(websiteContent, `${formattedResponse}`, 2) // Adjust the speed as needed
-
-        // await time.sleep(1000)
-        // requestRaw("https://api.codetabs.com/v1/proxy?quest="+website)
-        //     .then(response => response.text())
-        //     .then(response => {
-        //         const commandDiv = dcreate("div", "", `root:~# curl <span style='color: var(--accent-secondary); font-family: monospace;'>${website}</span>`);
-        //         parent.append(commandDiv);
-
-        //         // Simulate typing effect
-        //         let websiteContent = dcreate("pre");
-        //         websiteContent.style.color = "var(--text-dim)";
-        //         websiteContent.style.fontFamily = "monospace";
-        //         parent.append(websiteContent);
-
-        //         let formattedResponse = formatResponse(response.slice(0, 150).trim());
-        //         is_done_loading = false;
-        //         console.log(formattedResponse);
-        //         typeEffect(websiteContent, `"${formattedResponse}..."`, 20).then(() => {
-        //             is_done_loading = true;
-        //         }); // Adjust the speed as needed
-
-        //         parent.scrollTop = parent.scrollHeight;
-        //     });
-        // await time.sleep(1000)
     }
 }
 
@@ -82,6 +50,7 @@ function formatResponse(response) {
 async function typeEffect(element, text, delay) {
     const parent = element.parentElement;
 
+    // Simulate typing effect
     for (let char of text) {
         element.innerHTML += char;
         parent.scrollTop = parent.scrollHeight
