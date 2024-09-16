6ca42a61a053a1177025845a6792d4ff74534529
wa1ker38552 <walker.alt.38552@gmail.com>
2024-09-15 20:39:53 -0700

    scrollbars, fixed scrolling

--- js/modals.js
+++ js/modals.js
@@ -1,11 +1,13 @@
 // Function to open a modal
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden"
 }
 
 // Function to close a modal
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = ""
 }
 
 // Close modal when user clicks outside the modal content
